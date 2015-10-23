13519e1955e349e4ddcfe688dff882193513058e
martinjoconnor <martin.oconnor@stanford.edu>
2015-10-22 17:01:41 -0700

    Prelimninary GEOmetadb wiring

---
+++ src/main/java/org/metadatacenter/ingestors/geo/metadb/GEOmetadbNames.java
@@ -0,0 +1,80 @@
+package org.metadatacenter.ingestors.geo.metadb;
+
+/**
+ * http://gbnci.abcc.ncifcrf.gov/geo/geo_help.php
+ */
+public class GEOmetadbNames
+{
+  public static final String SERIES_TABLE_NAME = "GSE";
+  public static final String SAMPLE_TABLE_NAME = "GSM";
+  public static final String PLATFORM_TABLE_NAME = "GPL";
+
+  public static final String SERIES_TABLE_TITLE_COLUMN_NAME = "title";
+  public static final String SERIES_TABLE_GSE_COLUMN_NAME = "gse";
+  public static final String SERIES_TABLE_STATUS_COLUMN_NAME = "status";
+  public static final String SERIES_TABLE_SUBMISSION_DATE_COLUMN_NAME = "submission_date";
+  public static final String SERIES_TABLE_LAST_UPDATE_DATE_COLUMN_NAME = "last_update_date";
+  public static final String SERIES_TABLE_PUBMED_ID_COLUMN_NAME = "pubmed_id";
+  public static final String SERIES_TABLE_SUMMARY_COLUMN_NAME = "summary";
+  public static final String SERIES_TABLE_TYPE_COLUMN_NAME = "type";
+  public static final String SERIES_TABLE_CONTRIBUTOR_COLUMN_NAME = "contributor";
+  public static final String SERIES_TABLE_CONTACT_COLUMN_NAME = "contact";
+  public static final String SERIES_TABLE_WEB_LINK_COLUMN_NAME = "web_link";
+  public static final String SERIES_TABLE_OVERALL_DESIGN_COLUMN_NAME = "overall_design";
+  public static final String SERIES_TABLE_REPEATS_COLUMN_NAME = "repeats";
+  public static final String SERIES_TABLE_REPEATS_SAMPLE_LIST_COLUMN_NAME = "repeats_sample_list";
+  public static final String SERIES_TABLE_VARIABLE_COLUMN_NAME = "variable";
+  public static final String SERIES_TABLE_VARIABLE_DESCRIPTION_COLUMN_NAME = "variable_description";
+  public static final String SERIES_TABLE_SUPPLEMENTARY_FILE_COLUMN_NAME = "supplementary_file";
+
+  public static final String SAMPLE_TABLE_TITLE_COLUMN_NAME = "title";
+  public static final String SAMPLE_TABLE_GSM_COLUMN_NAME = "gsm";
+  public static final String SAMPLE_TABLE_SERIES_ID_COLUMN_NAME = "series_id";
+  public static final String SAMPLE_TABLE_GPL_COLUMN_NAME = "gpl";
+  public static final String SAMPLE_TABLE_STATUS_COLUMN_NAME = "status";
+  public static final String SAMPLE_TABLE_SUBMISSION_DATECOLUMN_NAME = "submission_date";
+  public static final String SAMPLE_TABLE_LAST_UPDATE_DATE_COLUMN_NAME = "last_update_date";
+  public static final String SAMPLE_TABLE_TYPE_COLUMN_NAME = "type";
+  public static final String SAMPLE_TABLE_CHANNEL_COUNT_COLUMN_NAME = "channel_count";
+  public static final String SAMPLE_TABLE_SOURCE_NAME_CH1_COLUMN_NAME = "source_name_ch1";
+  public static final String SAMPLE_TABLE_CHARACTERISTIC_CH1_COLUMN_NAME = "characteristics_ch1";
+  public static final String SAMPLE_TABLE_MOLECULE_CH1_COLUMN_NAME = "molecule_ch1";
+  public static final String SAMPLE_TABLE_LABEL_CH1_COLUMN_NAME = "label_ch1";
+  public static final String SAMPLE_TABLE_TREATMENT_PROTOCOL_CH1_COLUMN_NAME = "treatment_protocol_ch1";
+  public static final String SAMPLE_TABLE_EXTRACT_PROTOCOL_CH1_COLUMN_NAME = "extract_protocol_ch1";
+  public static final String SAMPLE_TABLE_LABEL_PROTOCOL_CH1_COLUMN_NAME = "label_protocol_ch1";
+  public static final String SAMPLE_TABLE_SOURCE_NAME_CH2_COLUMN_NAME = "source_name_ch2";
+  public static final String SAMPLE_TABLE_CHARACTERISTIC_CH2_COLUMN_NAME = "characteristics_ch2";
+  public static final String SAMPLE_TABLE_MOLECULE_CH2_COLUMN_NAME = "molecule_ch2";
+  public static final String SAMPLE_TABLE_LABEL_CH2_COLUMN_NAME = "label_ch2";
+  public static final String SAMPLE_TABLE_TREATMENT_PROTOCOL_CH2_COLUMN_NAME = "treatment_protocol_ch2";
+  public static final String SAMPLE_TABLE_EXTRACT_PROTOCOL_CH2_COLUMN_NAME = "extract_protocol_ch2";
+  public static final String SAMPLE_TABLE_LABEL_PROTOCOL_CH2_COLUMN_NAME = "label_protocol_ch2";
+  public static final String SAMPLE_TABLE_COLUMN_NAME = "hyb_protocol";
+  public static final String SAMPLE_TABLE_DESCRIPTION_COLUMN_NAME = "description";
+  public static final String SAMPLE_TABLE_DATA_PROCESSING_COLUMN_NAME = "data_processing";
+  public static final String SAMPLE_TABLE_CONTACT_COLUMN_NAME = "contact";
+  public static final String SAMPLE_TABLE_SUPPLEMENTARY_FILE_COLUMN_NAME = "supplementary_file";
+  public static final String SAMPLE_TABLE_DATA_ROW_COUNT_COLUMN_NAME = "data_row_count";
+
+  public static final String PLATFORM_TABLE_TITLE_COLUMN_NAME = "title";
+  public static final String PLATFORM_TABLE_GPL_COLUMN_NAME = "gpl";
+  public static final String PLATFORM_TABLE_STATUS_COLUMN_NAME = "status";
+  public static final String PLATFORM_TABLE_SUBMISSION_DATE_COLUMN_NAME = "submission_date";
+  public static final String PLATFORM_TABLE_LAST_UPDATE_DATE_COLUMN_NAME = "last_update_date";
+  public static final String PLATFORM_TABLE_TECHNOLOGY_COLUMN_NAME = "technology";
+  public static final String PLATFORM_TABLE_DISTRIBUTION_COLUMN_NAME = "distribution";
+  public static final String PLATFORM_TABLE_ORGANISM_COLUMN_NAME = "organism";
+  public static final String PLATFORM_TABLE_MANUFACTURER_COLUMN_NAME = "manufacturer";
+  public static final String PLATFORM_TABLE_MANUFACTURER_PROTOCOL_COLUMN_NAME = "maufacturer_protocol";
+  public static final String PLATFORM_TABLE_COATING_COLUMN_NAME = "coating";
+  public static final String PLATFORM_TABLE_CATALOG_NUMBER_COLUMN_NAME = "catalog_number";
+  public static final String PLATFORM_TABLE_SUPPORT_COLUMN_NAME = "support";
+  public static final String PLATFORM_TABLE_DESCRIPTION_COLUMN_NAME = "description";
+  public static final String PLATFORM_TABLE_WEB_LINK_COLUMN_NAME = "web_link";
+  public static final String PLATFORM_TABLE_CONTAXT_COLUMN_NAME = "contact";
+  public static final String PLATFORM_TABLE_DATA_ROW_COUNT_COLUMN_NAME = "data_row_count";
+  public static final String PLATFORM_TABLE_SUPPLEMENTARY_FILE_COLUMN_NAME = "supplementary_file";
+  public static final String PLATFORM_TABLE_BIOC_PACKAGE_COLUMN_NAME = "bioc_package";
+
+}
